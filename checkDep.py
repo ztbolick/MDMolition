17ef909145a881f6c81be02b4d311f3cdf58ce0c
Zac Bolick <zac.bolick@interlaced.io>
2021-07-21 13:47:13 -0700

    Added a util for static methods not sure if I'll use this

---
+++ checkDep.py
@@ -0,0 +1,11 @@
+import os.path
+import subprocess
+
+class dep:
+    def __init__(self):
+        self.dep = None
+        self.mdm = None
+
+
+def check_mdm(self):
+    pass
--- main.py
+++ main.py
@@ -4,7 +4,14 @@ import subprocess
 
 
 if __name__ == '__main__':
-    print("Starting MdMolition")
-    jamf = Jamf()
-    addigy = Addigy()
-    print(addigy.elements)
+    # print("Starting MdMolition")
+    # jamf = Jamf()
+    # addigy = Addigy()
+    # print(addigy.elements)
+
+    arg = "which jamf"
+    arg = arg.split()
+
+    checkShell = subprocess.run(arg, capture_output=True, text=True).stdout
+    
+    print(checkShell)
